e5ef4eb65ab9004ad1faf1723be34087baa5b552
zhangb <iloveapi@163.com>
2018-12-17 00:08:41 +0800

    update

---
+++ demo-groovy/src/StringExample.groovy
@@ -0,0 +1,12 @@
+/**
+ * StringExample
+ *
+ * @Author: zhangb
+ * @Date: 2018/11/16 21:03
+ */
+class StringExample {
+    void hello(){
+        println 'hello';
+    }
+
+}
---
+++ demo-groovy/src/main.groovy
@@ -0,0 +1,8 @@
+/**
+ * Controller
+ *
+ * @Author: zhangb
+ * @Date: 2018/12/16 21:08
+ */
+str = new  StringExample();
+str.hello();
